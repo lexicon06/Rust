b78466ec0310074e77704c99d5f7042506e1583e
lexicon06 <pablo.hehe2@gmail.com>
2025-07-17 21:40:22 -0300

    Fixed some bad practices, thanks to rust-analyzer extension

--- keyboard/src/main.rs
+++ keyboard/src/main.rs
@@ -5,10 +5,10 @@ fn main() {
     let mut user_name = String::new(); // new string = ""
     let mut user_age = String::new();
     //Result Success or Failed
-    let _ = io::stdin().read_line(&mut user_name).expect("Something went wrong, can't catch your name"); // we use ref ~ puntero, w "mut" we tell is able to write as well
+    io::stdin().read_line(&mut user_name).expect("Something went wrong, can't catch your name"); // we use ref ~ puntero, w "mut" we tell is able to write as well
     let user_name = user_name.trim();// delete odd characters w shadowing
     println!("Enter your age:");
-    let _ = io::stdin().read_line(&mut user_age).expect("Something went wrong, can't catch your age");
+    io::stdin().read_line(&mut user_age).expect("Something went wrong, can't catch your age");
     let user_age = user_age.trim();
     //success or failed result:
     let age:i32 = user_age.parse().unwrap();//we parse our number to real int32
